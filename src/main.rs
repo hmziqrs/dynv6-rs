c46c4356464c8f22a5226a59885a3b550dc7c498
hmziqrs <hmziqrs@gmail.com>
2025-03-22 14:31:15 +0500

    Add pnet dependency and implement IPv6 address detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use std::net::IpAddr;
+
+fn main() -> Result<(), Error> {
+    let interfaces = pnet::datalink::interfaces();
+
+    for iface in interfaces {
+        for ip in iface.ips {
+            if let IpAddr::V6(ipv6) = ip.ip() {
+                if !ipv6.is_loopback() && !ipv6.is_unicast_link_local() {
+                    println!("IPv6 Address: {}", ipv6);
+                    return Ok(());
+                }
+            }
+        }
+    }
+
+    println!("No valid global IPv6 address found.");
+    Ok(())
 }
